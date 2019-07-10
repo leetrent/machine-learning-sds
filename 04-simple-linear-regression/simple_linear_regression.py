fd17edd731bbee75f368fc333e9b81604606590d
LeeTrent <lee.trent.1@gmail.com>
2019-07-10 09:03:22 -0500

    Completes Simple Linear Regression in Python (Steps 2 and 3)

--- 04-simple-linear-regression/simple_linear_regression.py
+++ 04-simple-linear-regression/simple_linear_regression.py
@@ -13,25 +13,42 @@ import pandas as pd
 # Importing the dataset:
 ################################################################################
 dataset = pd.read_csv('Salary_Data.csv')
-print(dataset)
+#print(dataset)
 
 ################################################################################
 # Independent Variable (# of years of experience)(X)
 ################################################################################
-X = dataset.iloc[:, 0].values
-#X = dataset.iloc[:, -1].values
-print("")
-print(X)
+#X = dataset.iloc[:, 0].values
+X = dataset.iloc[:, :-1].values
+#print("")
+#print(X)
 
 ################################################################################
 # Dependent Variable (Salary)(Y)
 ################################################################################
 Y = dataset.iloc[:, 1].values
-print("")
-print(Y)
+#print("")
+#print(Y)
 
 ################################################################################
 # Splitting the dataset into the Training datset and the Test dataset:
 ################################################################################
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 1/3, random_state = 0)
+
+################################################################################
+# Fitting Simple Linear Regression to the Training Set:
+################################################################################
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+################################################################################
+# Predicting the Test Set Results:
+################################################################################
+Y_pred = regressor.predict(X_test)
+print(Y_pred)
+
+
+
+
